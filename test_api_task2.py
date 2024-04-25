e3bdaeca9a7089b56ea52f0e00a340936d4d875c
Nikhil Anand <nikhilanand@smail.iitm.ac.in>
2024-04-26 00:10:13 +0530

    Added unit tests

---
+++ test_api_task2.py
@@ -0,0 +1,25 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import FastAPI, UploadFile, File
+from task2 import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+def test_root_endpoint(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {}
+
+
+def test_predict_endpoint(client):
+    image_path = "/Users/nikhilanand/FastAPI_BDL_Assignment/five.png"
+
+    with open(image_path,'rb') as file:
+        file_content=file.read()
+    files = {"file": ("file.jpg", file_content)}
+
+    resp = client.post("/predict",files=files)
+    assert resp.status_code == 200
+    assert resp.json() == {"digit": "5"}
